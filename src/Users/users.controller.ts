7cadd6d7b82f06aa3f746a874971bd303b3ac157
saurabh <saurabh.solanki@kevit.io>
2025-08-13 18:45:39 +0530

    user apis for crud created but still left for checking and authorization still left

--- src/Users/users.controller.ts
+++ src/Users/users.controller.ts
@@ -1,9 +1,34 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
 import { UsersService } from "./users.service";
+import { UsersDto } from "./users.dto";
+import mongoose from "mongoose";
 
 @Controller('users')
 export class UsersControl {
     constructor(private usersService: UsersService) {}
 
+    @Post()
+    addUser(@Body() userDto: UsersDto) {
+        return this.usersService.addUser(userDto);
+    }
 
+    @Get()
+    getUser() {
+        return this.usersService.getAllUsers();
+    }
+
+    @Get('/:userId')
+    getUserById(@Param('userId') userId: mongoose.Schema.Types.ObjectId) {
+        return this.usersService.getUserById(userId);
+    }
+
+    @Delete('/:userId')
+    deleteUserById(@Param('userId') userId: mongoose.Schema.Types.ObjectId) {
+        return this.usersService.deleteUser(userId);
+    }
+
+    @Put('/:userId')
+    editUserById(@Param('userId') userId: mongoose.Schema.Types.ObjectId) {
+        return this.usersService.editUser(userId);
+    }
 }
---
+++ src/Users/users.dto.ts
@@ -0,0 +1,31 @@
+import { IsEmail, IsNotEmpty, IsString, IsNumber, IsOptional, IsBoolean, IsDate } from "class-validator";
+import mongoose from "mongoose";
+
+export class UsersDto {
+
+    _id: mongoose.Schema.Types.ObjectId;
+
+    @IsEmail()
+    @IsNotEmpty()  
+    email: string;
+
+    @IsString()
+    @IsNotEmpty()
+    password: string;
+
+    @IsString()
+    @IsNotEmpty()
+    role: string;
+
+    @IsNumber()
+    @IsNotEmpty() 
+    workspace_id: number;
+
+    @IsOptional()
+    @IsBoolean()  
+    isAdmin?: boolean
+
+    @IsDate()
+    createdAt: Date;
+
+}
