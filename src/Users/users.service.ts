1aa874d6137e8485990019e694c04be7ad0f95ef
saurabh <saurabh.solanki@kevit.io>
2025-08-14 12:29:25 +0530

    users password hashed and left for checking

--- src/Users/users.service.ts
+++ src/Users/users.service.ts
@@ -3,20 +3,29 @@ import { InjectModel } from "@nestjs/mongoose";
 import { UsersSchema } from "./users.schema";
 import mongoose, { Model } from "mongoose";
 import { updateUserDto, UsersDto } from "./users.dto";
+import * as bcrypt from "bcrypt";
 
 @Injectable()
 export class UsersService {
     constructor(@InjectModel(UsersSchema.name) private usersModel: Model<UsersSchema>) { }
 
-    //Add users--------------------------------------------------------------------
-    async addUser(addUser: UsersDto) {
+    //Add users-----------------------------------------------------------------------
+    async addUser({ password, ...userData }: UsersDto) {
         try {
-            const foundUser = await this.usersModel.findOne({ $and: [{ email: addUser.email }, { workspaceId: addUser.workspaceId }] }).exec();
+            const foundUser = await this.usersModel.findOne({ $and: [{ email: userData.email }, { workspaceId: userData.workspaceId }] }).exec();
             if (foundUser) throw new ConflictException; //409
-            const newUser = new this.usersModel(UsersDto);
-            const savedUser = await newUser.save();
-            const { isAdmin, ...savedUserDto } = savedUser;
-            return { ...savedUserDto };
+            bcrypt.hash(password, 10, async (e, hashedPass) => {
+                if (e) {
+                    console.log(e);
+                    throw new InternalServerErrorException;
+                }
+                else {
+                    const newUser = new this.usersModel({ password: hashedPass, ...userData });
+                    const savedUser = await newUser.save();
+                    const { isAdmin, ...savedUserDto } = savedUser;
+                    return { ...savedUserDto };
+                }
+            })
         }
         catch (err) {
             console.log(err);
@@ -24,7 +33,7 @@ export class UsersService {
         }
     }
 
-    //Get all users---------------------------------------------------------------
+    //Get all users------------------------------------------------------------------
     async getAllUsers() {
         try {
             const allUsers = await this.usersModel.find({}, { _id: 1, email: 1, role: 1, workspaceId: 1 }).exec();
@@ -36,7 +45,7 @@ export class UsersService {
         }
     }
 
-    //Get users by id--------------------------------------------------------------
+    //Get users by id----------------------------------------------------------------
     async getUserById(userId: mongoose.Schema.Types.ObjectId) {
         try {
             const findUser = await this.usersModel.findOne({ _id: userId }, { _id: 1, email: 1, role: 1, workspaceId: 1 }).exec();
@@ -49,7 +58,7 @@ export class UsersService {
         }
     }
 
-    //delete a user-------------------------------------------------------------
+    //delete a user-----------------------------------------------------------------
     async deleteUser(userId: mongoose.Schema.Types.ObjectId) {
         try {
             const deletedUser = await this.usersModel.findOneAndDelete({ _id: userId }, { returnDocument: "after" }).exec()
@@ -62,7 +71,7 @@ export class UsersService {
         }
     }
 
-    //edit a user-----------------------------------------------------------
+    //edit a user---------------------------------------------------------------------
     async editUser(userId: mongoose.Schema.Types.ObjectId, updateUserDto: updateUserDto) {
         try {
             const editedUser = await this.usersModel.findOneAndUpdate({ _id: userId }, { updateUserDto }, { returnDocument: "after" }).exec();
