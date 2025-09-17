0b88f325435a7573bc108cdf59fbb73883b015a0
saurabh <saurabh.solanki@kevit.io>
2025-09-17 20:02:58 +0530

    seperate endpoint of login created for user and admin

--- src/Auth/auth.controller.ts
+++ src/Auth/auth.controller.ts
@@ -4,12 +4,19 @@ import { AuthDto } from "./auth.dto";
 
 @Controller('login')
 export class AuthControl {
-    constructor(private authService: AuthService) {}
+    constructor(private authService: AuthService) { }
 
-    @Post()
-    async loginUser(@Body() authDto: AuthDto)  {
-       const token = await this.authService.loginUser(authDto);
-       if (!token?.token) throw new HttpException("Unauthorized Access", 401);
-       return token;
+    @Post('users')
+    async loginUser(@Body() authDto: AuthDto) {
+        const token = await this.authService.loginUser(authDto);
+        if (!token?.token) throw new HttpException("Unauthorized Access", 401);
+        return token;
+    }
+
+    @Post('admins')
+    async loginAdmin(@Body() authDto: AuthDto) {
+        const token = await this.authService.loginAdmin(authDto);
+        if (!token?.token) throw new HttpException("Unauthorized Access", 401);
+        return token;
     }
 }
